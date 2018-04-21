6edaee01e2fe428e7f611f174ffde072bed5ba97
Trent Stromkins <trent@smallrobot.org>
2018-04-20 22:04:40 -0700

    sri

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,10 +4,6 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
-    // 'gzip': {
-    //   extensions: ['js', 'css', 'svg'],
-    //   appendSuffix: false
-    // },
     'autoprefixer': {
       browsers: ['last 2 major version'],
       cascade: false
