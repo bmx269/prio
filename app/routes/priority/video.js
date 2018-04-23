d04a4c6b886e0bec676a8ad4590f28cefe90d3d2
Trent Stromkins <trent@smallrobot.org>
2018-04-23 13:12:54 -0700

    video

--- app/routes/priority/video.js
+++ app/routes/priority/video.js
@@ -6,17 +6,6 @@ export default Route.extend({
   model() {
     return RSVP.hash({
       page: this.store.findRecord('page', '01c32a04-d5a7-4c55-b6b0-e83484860fce'),
-      // page: this.store.query('page', {
-      //   filter:
-      //     {
-      //       'slug':{
-      //         'value': '/priority'
-      //       },
-      //     },
-      // })
-      //   .then(pages => {
-      //     return pages.get('firstObject');
-      //   }),
     });
   },
 
