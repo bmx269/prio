5bee1cc12d06df4d0cbcba41b72fcfddedcc7a38
Trent Stromkins <trent@smallrobot.org>
2018-04-24 11:17:30 -0700

    our story model change

--- app/routes/about/our-story.js
+++ app/routes/about/our-story.js
@@ -4,14 +4,7 @@ import RSVP from 'rsvp';
 export default Route.extend({
 
   model() {
-    return RSVP.hash({
-      page: this.store.findRecord('page', '870716c3-1d83-49c7-94cb-5e114342e6bb'),
-      snapshots: this.store.findAll('snapshot')
-    });
+    return this.store.findRecord('page', '870716c3-1d83-49c7-94cb-5e114342e6bb');
   },
 
-  setupController(controller, models) {
-    controller.set('page', models.page);
-    controller.set('snapshots', models.snapshots);
-  }
 });
