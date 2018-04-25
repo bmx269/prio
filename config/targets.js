103a8024819564afe5a36e4c26655a697cc156ef
Trent Stromkins <trent@smallrobot.org>
2018-04-25 10:17:19 -0700

    update targets

--- config/targets.js
+++ config/targets.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const browsers = [
+  'ie 11',
+  'last 2 edge versions',
   'last 2 Chrome versions',
   'last 2 Firefox versions',
   'last 2 Safari versions'
--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,7 +5,13 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
     'autoprefixer': {
-      browsers: ['last 2 major version'],
+      browsers: [
+        'ie 11',
+        'last 2 edge versions',
+        'last 2 Chrome versions',
+        'last 2 Firefox versions',
+        'last 2 Safari versions'
+      ],
       cascade: false
     },
     'ember-bootstrap': {
