d0ed2838a7644f68a323098a42d7d8fcc416a376
Trent Stromkins <trent@smallrobot.org>
2018-09-14 16:45:12 -0700

    thumbnail

--- app/models/article.js
+++ app/models/article.js
@@ -12,4 +12,5 @@ export default DS.Model.extend({
   useDownload: DS.attr('boolean'),
   archived: DS.attr('boolean'),
   file: DS.belongsTo('file', { async: true }),
+  thumbnail: DS.belongsTo('file', { async: true })
 });
