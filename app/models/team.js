2d518bcc164dd8e6169049a6a5e8ea6d58bf753f
Trent Stromkins <trent@smallrobot.org>
2019-01-17 10:54:25 -0800

    team ordering

--- app/models/team.js
+++ app/models/team.js
@@ -11,6 +11,7 @@ export default DS.Model.extend({
   email: DS.attr('string'),
   linkedin: DS.attr('string'),
   dashedTitle: DS.attr('string'),
+  order: DS.attr('number'),
   body: DS.attr(''),
   image: DS.belongsTo('file', { async: true }),
   thumbnail: DS.belongsTo('file', { async: true }),
