3c7402564731206649453e1926918ffdc9b8593f
Trent Stromkins <trent@smallrobot.org>
2019-02-13 15:20:59 -0800

    remove homepage new site statement

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,5 +1,17 @@
 import Route from '@ember/routing/route';
+import RSVP from 'rsvp';
 
 export default Route.extend({
+
+  model() {
+    return RSVP.hash({
+      page: this.store.findRecord('page', 'af5ab178-3835-4eb9-9f8a-a79f7e7268bb'),
+    });
+  },
+
+  setupController(controller, models) {
+    controller.set('page', models.page);
+  }
+
 
 });
