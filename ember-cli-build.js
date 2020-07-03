ea8a93ac3e5084baa625df9418af7f1b13f85087
Trent Stromkins <trent@smallrobot.org>
2020-07-03 14:20:33 -0700

    remove fastboot

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -12,33 +12,31 @@ module.exports = function(defaults) {
       'bootstrapVersion': 4,
       'importBootstrapFont': false,
       'importBootstrapCSS': false
-    },
-    'ember-cli-babel': {
-      includePolyfill: true
-    },
-    'prember': {
-      baseRoot: 'https://priowealth.com',
-      urls: [
-        '/',
-        '/about',
-        '/about/our-story',
-        '/about/how-different',
-        '/about/your-team',
-        '/financial-life',
-        '/financial-life/clarity',
-        '/investment',
-        '/investment/management',
-        '/connections',
-        '/connections/blog',
-        '/connections/in-the-news',
-        '/connections/investment-updates',
-        '/priority',
-        '/priority/video',
-        '/contact',
-        '/contact/form',
-        '/disclosure'
-      ]
-    }
+    } //,
+
+    // 'prember': {
+    //   baseRoot: 'https://priowealth.com',
+    //   urls: [
+    //     '/',
+    //     '/about',
+    //     '/about/our-story',
+    //     '/about/how-different',
+    //     '/about/your-team',
+    //     '/financial-life',
+    //     '/financial-life/clarity',
+    //     '/investment',
+    //     '/investment/management',
+    //     '/connections',
+    //     '/connections/blog',
+    //     '/connections/in-the-news',
+    //     '/connections/investment-updates',
+    //     '/priority',
+    //     '/priority/video',
+    //     '/contact',
+    //     '/contact/form',
+    //     '/disclosure'
+    //   ]
+    // }
   });
 
   // Use `app.import` to add additional libraries to the generated
