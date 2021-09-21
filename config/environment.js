cb7895f4e635e9fad0e8d95c446e7481072bf771
Trent Stromkins <trent@smallrobot.org>
2021-09-20 17:41:34 -0700

    API location

--- config/environment.js
+++ config/environment.js
@@ -5,7 +5,7 @@ module.exports = function(environment) {
     modulePrefix: 'prio',
     environment,
     rootURL: '/',
-    host: 'https://api.priowealth.com', //drupal
+    host: 'https://prio.smallrobot.co', //drupal
     namespace: 'api',
     locationType: 'router-scroll',
     historySupportMiddleware: true,
